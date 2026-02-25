18d24994bc3e42c2835c01789689ae348d403a7f
Leanid Kanoplich <ceo@swapoil.de>
2026-02-26 00:10:11 +0100

    d_c_gitkeep_task24.md

---
+++ prisma/migrations/20260225230705_journal_document_indexes/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "journal_entries_documentType_idx" ON "journal_entries"("documentType");
+
+-- CreateIndex
+CREATE INDEX "journal_entries_documentId_idx" ON "journal_entries"("documentId");
